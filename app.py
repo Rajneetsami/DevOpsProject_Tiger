3532c61add6dbe3daf634825837d74dae446ffae
Rajneet kaur <126429338+Rajneetsami@users.noreply.github.com>
2024-11-25 18:57:21 +0100

    app.py

--- app.py
+++ app.py
@@ -53,4 +53,4 @@ def update_weather():
     return jsonify({"error": "Failed to fetch weather data"}), 500
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=False)
+    app.run(host="0.0.0.0", port=5001, debug=False)
