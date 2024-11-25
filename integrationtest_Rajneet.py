45b2597ce71554fd4ebec4c75dd9b1dd6df84f67
Rajneet kaur <126429338+Rajneetsami@users.noreply.github.com>
2024-11-25 18:04:50 +0100

    integrationtest_Rajneet.py

--- integrationtest_Rajneet.py
+++ integrationtest_Rajneet.py
@@ -5,24 +5,18 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-def test_weather_api():# Test the OpenWeather API and verify essential fields in the response."""
-    
-    # Load the API key from environment variables
+def test_weather_api():
     api_key = os.getenv('API_KEY')
     if not api_key:
         raise ValueError("API key is missing in the .env file")
 
-    # Construct the API URL using coordinates from app.py
     url = f"https://api.openweathermap.org/data/2.5/weather?lat={LAT}&lon={LON}&appid={api_key}&units=metric&lang=en"
     
-    # Send GET request to OpenWeather API
     response = requests.get(url)
-
-    # Check the response status code
+    
     if response.status_code == 200:
         print("Integration test passed: Received OK status from OpenWeather API")
 
-        # Verify that expected fields are present in the JSON response
         json_data = response.json()
         required_fields = ['name', 'main', 'weather', 'wind', 'sys']
         
